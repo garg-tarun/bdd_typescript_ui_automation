06c1dcbdb519ef630e42b62847b68856228826bd
Tarun Garg <tarun@Taruns-MacBook-Pro.local>
2025-01-03 08:52:50 +1100

    Enabled parallel execution of tests

---
+++ src/features/image_generation_copy.feature
@@ -0,0 +1,16 @@
+@regression
+Feature: Image Generation Functionality
+    @smoke
+    Scenario: Verify user can navigate to Create Your Custom Scrambler Ducati page
+        Given I am on the Ducati Scrambler website
+        When I click "Start to Create"
+        Then I should see the "Create Your Custom Scrambler Ducati" page
+        Given I am on the image creation page
+        When I fill in the prompt and click "Generate"
+        And  I wait for the generation process to complete
+        Then I should see the 4 generated images 
+        Given the 4 images have been generated and are visible
+        When I fill in my details and accept the terms
+        And I click "Submit"
+        Then I should be able to choose one of the 4 images
+        And the resolution of the saved file should be 2056 x 1368
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,42 @@
 import { exec } from "child_process";
 
-// Initialise a common_arguments command string for running cucumber tests
-const common_arguments = `./src/features/*.feature \
+//Define a common command string for running cucumber tests
+const common = `./src/features/*.feature \
   --require-module ts-node/register \
   --require ./src/step-definitions/**/**/*.ts \
   --require src/hooks/**/*.ts \
   --format json:./reports/report.json \
   --format html:./reports/report.html  `;
 
-// Define an interface for the test profiles using tags 
-interface TestProfiles {
+//Define an interface for the profiles object
+//It defines an interface where each key is a string and its value is also a string
+interface ProfileCommands {
     [key: string]: string;
 }
 
-// Define a command strings for different test profiles
-const profiles: TestProfiles = {
-    smoke: `${common_arguments} --tags "@smoke"`,
-    regression: `${common_arguments} --tags "@regression"`,
+//Define a command strings for different test profiles
+const profiles: ProfileCommands = {
+    smoke: `${common} --tags "@smoke"`,
+    regression: `${common} --tags "@regression"`,
+    login: `${common} --tags "@login"`,
+    contactUs: `${common} --tags "@contact-us"`,
 }
 
-// Fetch the test tag as third command-line argument and assign it to the profile
+//Get the third command-line argument and assign it to the profile
+//i.e. smoke, regression etc
 const profile = process.argv[2];
 
-// Construct the complete command based on the selected profile 
-// to run the tests for the selected profile
-let command = `npx cucumber-js ${profiles[profile as 'smoke' | 'regression']}`;
+// Initialise parallel to run tests in multiple browser for different feature files
+const parallel = process.argv[3] ? process.argv[3] : 1; 
 
-// Print the command
+//Construct the command string based on the selected profile
+//command is the full command to run the tests for the selected profile
+let command = `npx cucumber-js ${profiles[profile as 'smoke' | 'regression' | 'login' | 'contact-us']} --parallel ${parallel}`;
+
+//Print the constructed command
 console.log(command);
 
-// Execute the command
+//Execute the command
 exec(command, { encoding: 'utf-8'}, (error: Error | null, stdout: string) =>{
   //Log the output of the command
   console.log(stdout);
